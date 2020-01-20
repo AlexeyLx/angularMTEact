956a811f6337670c851bb6360fa7eb59281e013d
AlexeyLx <43266953+AlexeyLx@users.noreply.github.com>
2020-01-20 14:11:41 +0200

    perf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+ 
 import { AppComponent } from './app.component';
-
+ 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { 
+  MatTableModule, 
+  MatDialogModule, 
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule} from '@angular/material';
+ 
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  entryComponents: [
+    DialogBoxComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
